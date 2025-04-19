6bc0f4fe8b2804ba90e4bade0181134a8eb72802
Marcello Cordeiro <23259818+marcellocordeiro@users.noreply.github.com>
2025-04-19 03:22:18 -0300

    Compiler defines example

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,14 @@
 #include <string_view>
 #include <vector>
 
+#if defined(__clang__)
+// clang
+#elif defined(__GNUC__) || defined(__GNUG__)
+// gcc
+#elif defined(_MSC_VER)
+// MSVC
+#endif
+
 void withSpan(std::span<std::string_view> args) {
   (void)args;
 }
